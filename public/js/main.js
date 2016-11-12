1400b40e1c88db98f93375e08237fbefeaa4b303
gonzaloterzano@gmail.com <gonzaloterzano@gmail.com>
2016-11-12 12:07:01 -0300

    js & css main

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+ // Initialize Firebase
+ var config = {
+ 	apiKey: "AIzaSyC4MgmGtRsDXbl20apZYtiKcZo34oqIP3g",
+ 	authDomain: "bancoalimentos-db03d.firebaseapp.com",
+ 	databaseURL: "https://bancoalimentos-db03d.firebaseio.com",
+ 	storageBucket: "bancoalimentos-db03d.appspot.com",
+ 	messagingSenderId: "794591350661"
+ };
+ firebase.initializeApp(config);
