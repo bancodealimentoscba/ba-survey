18c61289e112aa44bfbcb7afe1c9447f07b4a572
Gonzalo Terzano <gonzalo.terzano@globant.com>
2016-11-22 14:48:32 -0300

    addOrganization

--- public/app/components/form/formController.js
+++ public/app/components/form/formController.js
@@ -1,28 +1,53 @@
-baSurvey.controller('formCtrl', ['$scope', '$timeout', '$firebaseObject', 
+baSurvey.controller('formCtrl', ['$scope', '$timeout', '$firebaseObject',
 	function($scope, $timeout, $firebaseObject) {
 		$scope.legalStatus = '';
-		$scope.organizationType = '';
-
+		$scope.organizacion = {
+			direccion: {},
+			contacto: {},
+			tipo: ''
+		};
+		$scope.personeria = {};
 
 		$scope.scrollTo = function(target) {
 			$('html, body').animate({
-				scrollTop: $('#'+target).offset().top
+				scrollTop: $('#' + target).offset().top
 			}, 1000);
 		}
 
 		$scope.onLegalStatusChange = function() {
-			$timeout(function(){
-				if ($scope.legalStatus === 'YES'){
+			$timeout(function() {
+				if ($scope.legalStatus === 'YES') {
 					$scope.scrollTo('two');
 				} else $scope.scrollTo('one');
 			}, 250)
 		}
 
-		 var database = firebase.database().ref();
+		var ref = firebase.database().ref();
 
-		 var firebaseObject = $firebaseObject(database);
-		 firebaseObject.$loaded().then(function(data){
-		 	console.log(data);
-		 });
+		var firebaseOrganizaciones = $firebaseObject(ref); //.child("organizaciones")
+		firebaseOrganizaciones.$loaded().then(function(data) {
+			console.log(data);
+		});
+
+		$scope.writeOrganization = function() {
+			var id = 'ORG' + ref.push().key;
+			var nuevaOrganizacion = {
+				id: id,
+				nombre: $scope.organizacion.nombre,
+				calle: $scope.organizacion.direccion.calle,
+				numero: $scope.organizacion.direccion.numero,
+				barrio:  $scope.organizacion.direccion.barrio,
+				referente:  $scope.organizacion.contacto.nombre,
+				caracteristica:  $scope.organizacion.contacto.caracteristica,
+				telefono:  $scope.organizacion.contacto.telefono,
+				email:  $scope.organizacion.contacto.email,
+				tipo: $scope.organizacion.tipo
+			}
+			firebaseOrganizaciones.organizaciones[id] = nuevaOrganizacion
+			var saveProcess = firebaseOrganizaciones.$save();
+			saveProcess.then(function(data){
+				debugger
+			});
+		}
 	}
-	])
+])
