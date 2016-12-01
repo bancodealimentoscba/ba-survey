d585315e3e5720cea74ed1a6520c00c39a559d54
MatiPruvost <matiaspruvost@gmail.com>
2016-12-01 20:32:57 -0300

    Update the org model in the form module

--- public/app/components/form/formController.js
+++ public/app/components/form/formController.js
@@ -1,11 +1,42 @@
-baSurvey.controller('formCtrl', ['$scope', '$timeout', 'database',
-	function($scope, $timeout, database) {
+baSurvey.controller('formCtrl', ['$scope', '$timeout', 'blockUI', 'database',
+	function($scope, $timeout, blockUI, database) {
 		$scope.legalStatus = '';
 		$scope.organization = {
 			address: {},
 			contact: {},
-			type: ''
+			type: '',
+			periodicity: {
+				onceWeek: false,
+				threeTimes: false,
+				everyday: false
+			},
+			services: {
+				breakfast: false,
+				lunch: false,
+				snak: false,
+				dinner: false
+			},
+			recipient: {
+				_0_2: 0,
+				_3_5: 0,
+				_6_12: 0,
+				teenagers: 0,
+				adults: 0,
+				elder: 0
+			},
+			schedule: {
+				morning: {
+					available: false
+				},
+				afternoon: {
+					available: false
+				},
+				night: {
+					available: false
+				},
+			}
 		};
+
 		$scope.personeria = {};
 
 		$scope.scrollTo = function(target) {
@@ -29,20 +60,72 @@ baSurvey.controller('formCtrl', ['$scope', '$timeout', 'database',
 		$scope.writeOrganization = function() {
 			var newOrganization = {
 				nombre: $scope.organization.name,
-				calle: $scope.organization.address.street,
-				numero: $scope.organization.address.number,
-				barrio:  $scope.organization.address.neighborhood,
-				localidad:  $scope.organization.address.city,
-				referente:  $scope.organization.contact.name,
-				fijo_caracteristica:  $scope.organization.contact.phone_prefix,
-				fijo_numero:  $scope.organization.contact.phone_number,
-				celular_caracteristica:  $scope.organization.contact.cellphone_prefix,
-				celular_numero:  $scope.organization.contact.cellphone_number,
-				email:  $scope.organization.contact.email,
-				tipo: $scope.organization.type
-			}
-			database.writeData('organizaciones', newOrganization)
-			
-		}
+				ubicacion: {
+					localidad: $scope.organization.address.city,
+					barrio: $scope.organization.address.neighborhood,
+					calle: $scope.organization.address.street,
+					numero: $scope.organization.address.number,
+					coordenadas: {
+						lat: 0,
+						lng: 0
+					}
+				},
+				tipo: $scope.organization.type,
+				contacto: {
+					nombre: $scope.organization.contact.name,
+					telefonos: {
+						celular: {
+	            caracteristica: $scope.organization.contact.cellphone_prefix,
+	            numero: $scope.organization.contact.cellphone_number
+          	},
+	          fijo: {
+	            caracteristica: $scope.organization.contact.phone_prefix,
+	            numero: $scope.organization.contact.phone_number
+	          }
+					},
+					email: $scope.organization.contact.email
+				},
+				beneficios: {
+	        desayuno: $scope.organization.services.breakfast,
+	        almuerzo: $scope.organization.services.lunch,
+	        merienda: $scope.organization.services.snak,
+	        cena: $scope.organization.services.dinner
+	      },
+	      personaria_juridica: {
+	        numero: $scope.organization.legalStatus
+	      },
+				beneficiarios: {
+	        "0_2": $scope.organization.recipient._0_2,
+	        "3_5": $scope.organization.recipient._3_5,
+	        "6_12": $scope.organization.recipient._6_12,
+	        adolescentes: $scope.organization.recipient.teenagers,
+	        adultos: $scope.organization.recipient.adults,
+	        ancianos: $scope.organization.recipient.elder
+	      },
+	      periodicidad: $scope.organization.periodicity,
+	      horario: {
+	        manana: {
+	          disponible: $scope.organization.schedule.morning.available,
+	          desde: $scope.organization.schedule.morning.from,
+	          hasta: $scope.organization.schedule.morning.to
+	        },
+	        tarde: {
+	          disponible: $scope.organization.schedule.afternoon.available,
+	          desde: $scope.organization.schedule.afternoon.from,
+	          hasta: $scope.organization.schedule.afternoon.to
+	        },
+	        noche: {
+	          disponible: $scope.organization.schedule.night.available,
+	          desde: $scope.organization.schedule.night.from,
+	          hasta: $scope.organization.schedule.night.to
+	        }
+				}
+			};
+			console.log(newOrganization);
+			blockUI.start();
+			database.writeData('organizaciones', newOrganization).finally(function(){
+	      blockUI.stop();
+	    });
+		};
 	}
 ])
