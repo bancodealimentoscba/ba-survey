45c628ea3e492ae7181ee207e76c3f834be2e46f
Gonzalo Terzano <gonzalo.terzano@globant.com>
2016-12-05 17:35:24 -0300

    ubicacion en google maps

--- public/app/components/editOrg/editOrgController.js
+++ public/app/components/editOrg/editOrgController.js
@@ -1,22 +1,35 @@
 baSurvey.controller('editOrgCtrl', ['$location', '$scope', '$timeout', 'blockUI', 'database',
   function($location, $scope, $timeout, blockUI, database) {
 
-    // $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-    // $scope.marker = {
-    //   coords: {
-    //     id: 0,
-    //     latitude: -40.1451,
-    //     longitude: -99.6680
-    //   }
-    // };
+    $scope.map = {
+      center: {
+        latitude: 45,
+        longitude: -73
+      },
+      zoom: 15
+    }
+
+    $scope.marker = {
+      id: 0,
+      options: {
+        draggable: true
+      },
+      coords: {
+        latitude: -40.1451,
+        longitude: -99.6680
+      }
+    }
 
     $scope.save = function() {
+      $scope.organizacion.ubicacion.coordenadas.lat = $scope.marker.coords.latitude;
+      $scope.organizacion.ubicacion.coordenadas.lng = $scope.marker.coords.longitude;
+
       blockUI.start();
       $timeout(function() {
         blockUI.stop();
         $location.path('orgsList');
       }, 2000);
-    };
+    }
 
     $scope.organizacion = {
       "estado": 1,
@@ -24,7 +37,7 @@ baSurvey.controller('editOrgCtrl', ['$location', '$scope', '$timeout', 'blockUI'
       "ubicacion": {
         "localidad": "cordoba",
         "barrio": "centro",
-        "calle": "colon",
+        "calle": "av colon",
         "numero": "135",
         "coordenadas": {
           "lat": 0,
@@ -36,12 +49,12 @@ baSurvey.controller('editOrgCtrl', ['$location', '$scope', '$timeout', 'blockUI'
         "nombre": "A B C",
         "telefonos": {
           "celular": {
-              "caracteristica": "351",
-              "numero": "4244444"
+            "caracteristica": "351",
+            "numero": "4244444"
           },
           "fijo": {
-              "caracteristica": "351",
-              "numero": "5555555"
+            "caracteristica": "351",
+            "numero": "5555555"
           }
         },
         "email": "r@fake.com"
@@ -81,7 +94,37 @@ baSurvey.controller('editOrgCtrl', ['$location', '$scope', '$timeout', 'blockUI'
           "hasta": "10"
         }
       }
-    };
+    }
+
+    if ($scope.organizacion.ubicacion.coordenadas.lng === 0 && $scope.organizacion.ubicacion.coordenadas.lng === 0) {
+
+      geocoder = new google.maps.Geocoder();
+      geocoder.geocode({
+        'address': $scope.organizacion.ubicacion.calle + " " + $scope.organizacion.ubicacion.numero + " " + $scope.organizacion.ubicacion.barrio + " " + $scope.organizacion.ubicacion.localidad + " Argentina"
+      }, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+
+          var location = {
+            latitude: results[0].geometry.location.lat(),
+            longitude: results[0].geometry.location.lng()
+          }
+
+          console.log(location);
+          $scope.map.center = location;
+
+          $scope.marker.coords.latitude = location.latitude;
+          $scope.marker.coords.longitude = location.longitude;
+
+        }
+      })
+    } else {
+      $scope.map.center.latitude = $scope.organizacion.ubicacion.coordenadas.lat;
+      $scope.map.center.longitude = $scope.organizacion.ubicacion.coordenadas.lng;
+
+      $scope.marker.coords.latitude = $scope.organizacion.ubicacion.coordenadas.lat;
+      $scope.marker.coords.longitude = $scope.organizacion.ubicacion.coordenadas.lng;
+    }
 
   }
 ])
