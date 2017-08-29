23ba44dd4eb8ecdadb424c62ff9becc201851882
Jose Ferrer <jose.ferrer@globant.com>
2017-08-29 14:44:15 -0300

    Se agrego nueva condig para apuntar a la db de DEV

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,17 @@
 (function (_firebase) {
   var config;
   
+  /*DEV Config*/
+  config = {
+    apiKey: "AIzaSyDGrj6ytausPcYn-0uZZ5LKWpSEqGQDn7E",
+    authDomain: "bancodealimentoscba-dev.firebaseapp.com",
+    databaseURL: "https://bancodealimentoscba-dev.firebaseio.com",
+    storageBucket: "bancodealimentoscba-dev.appspot.com",
+    messagingSenderId: "850994234398"
+  };  
+  
+  /*PROD Config
+
   config = {
     apiKey: "AIzaSyCgv9v7BUbJJYHhpz5dqweEwAqzzJDkYx0",
     authDomain: "bancodealimentoscba-1e703.firebaseapp.com",
@@ -8,6 +19,7 @@
     storageBucket: "bancodealimentoscba-1e703.appspot.com",
     messagingSenderId: "850994234398"
   };
+  */
 
   _firebase.initializeApp(config);
 })(window.firebase);
